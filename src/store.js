8021643721c5117be719364977fa13d549ffaa65
ThomasJd <thomasdinh25@gmail.com>
2018-07-26 23:08:25 -0500

    store.js

--- src/store.js
+++ src/store.js
@@ -5,3 +5,14 @@ import rootReducer from './reducers'
 const initialState = {}
 
 const middleware = [thunk]
+
+//need to spread middleware because we put it inside an array above
+const store = createStore(
+  rootReducer,
+  initialState(
+      applyMiddleware(...middleware),
+      window._REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
+
+export default store
